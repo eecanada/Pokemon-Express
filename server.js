319b431e5e8bb4b565e6d2c43f87e20f784c53bb
Eder Canada <canada.eder@gmail.com>
2019-10-15 16:56:59 -0700

    Commit 3 - index.ejs view rendered at pokemon route

--- server.js
+++ server.js
@@ -5,11 +5,22 @@ const Pokemon = require('./Models/pokemon.js');
 
 const PORT = 3000;
 
-app.get('/pokemon',(req,res)=>{
-  res.send(Pokemon)
+// app.get('/pokemon',(req,res)=>{
+//   res.send(Pokemon)
+// })
+
+
+
+app.get('/pokemon', (req,res)=>{
+  res.render('index.ejs',{
+    pokemon: Pokemon
+  });
 })
 
 
+// app.get('/pokemon/yo', (req,res)=>{
+//   res.send("Yooooooooo whatup")
+// })
 
 app.listen(PORT, () => {
     console.log("App is running on port:",
