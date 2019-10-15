46ea5de5d83433f3d66b861fa76525724b38c2bc
Eder Canada <canada.eder@gmail.com>
2019-10-15 16:19:27 -0700

    Commit 2 - Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 
-
+const Pokemon = require('./Models/pokemon.js');
 
 const PORT = 3000;
 
+app.get('/pokemon',(req,res)=>{
+  res.send(Pokemon)
+})
 
 
 
 app.listen(PORT, () => {
-  console.log("App is running on port: ", PORT);
+    console.log("App is running on port:",
+    PORT);
 });
